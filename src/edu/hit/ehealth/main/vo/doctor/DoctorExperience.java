cd40ed8fbe15e48651f3eae72a58de62e3bd0cbc
chenghengchao <hitchenghengchao@163.com>
2016-05-08 23:08:44 +0800

    Update DoctorExperience.java
    修改注解，使表名由拼音改为英文

--- src/edu/hit/ehealth/main/vo/doctor/DoctorExperience.java
+++ src/edu/hit/ehealth/main/vo/doctor/DoctorExperience.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "H_KBJY")
+@Table()
 public class DoctorExperience extends AbstractVOMD5 {
 
     private String personInfoID;
