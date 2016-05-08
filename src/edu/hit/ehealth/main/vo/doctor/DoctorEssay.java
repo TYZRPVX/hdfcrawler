f7f698679bcbc794df5e81f86c4c5f43c795c528
chenghengchao <hitchenghengchao@163.com>
2016-05-08 23:08:11 +0800

    Update DoctorEssay.java
    修改注解，使表名由拼音改为英文

--- src/edu/hit/ehealth/main/vo/doctor/DoctorEssay.java
+++ src/edu/hit/ehealth/main/vo/doctor/DoctorEssay.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "H_WZLB")
+@Table()
 public class DoctorEssay extends AbstractVOMD5 {
 
     private String homepageID;
