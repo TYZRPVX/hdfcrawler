5ea66811e9423cc27b4bb6d47f644fb8d2d73a63
chenghengchao <hitchenghengchao@163.com>
2016-05-08 23:14:41 +0800

    修改注解，使表名由拼音改为英文
    修改注解，使表名由拼音改为英文

--- src/edu/hit/ehealth/main/vo/patient/PatientServiceArea.java
+++ src/edu/hit/ehealth/main/vo/patient/PatientServiceArea.java
@@ -7,7 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "H_HZFWQLB")
+@Table()
 public class PatientServiceArea extends AbstractVOMD5 {
 
     private String homePageID;
